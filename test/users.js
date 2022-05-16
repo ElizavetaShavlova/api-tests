a79468b17f27fd832bd466f9c3b9608db18d66b9
ElizavetaShavlova <eliza260926@gmail.com>
2022-05-16 23:06:53 +0300

    add test: Get /users list and verify that it is not empty (using promise)

--- test/users.js
+++ test/users.js
@@ -7,11 +7,16 @@ const TOKEN =
     'a45e357c080a761f2194bc699f5ef27f1311fdaa50ce8af5658e92377bb42e54';
 
 describe('Users', () => {
-    it('Get /users', (done) => {
+    it('Get /users list and verify that it is not empty (using "done" callback)', (done) => {
         request.get(`users?access-token=${TOKEN}`).end((err, res) => {
-            console.log(err);
+            console.log("error: ", err);
             expect(res.body.data).to.not.be.empty;
             done();
+        });
+    });
+    it('Get /users list and verify that it is not empty (using promise)', () => {
+        return request.get(`users?access-token=${TOKEN}`).then((res) => {
+            expect(res.body.data).to.not.be.empty;
         });
     });
 });
