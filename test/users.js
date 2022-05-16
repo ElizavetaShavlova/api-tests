ff7468faff8db99d05d0c598cd3cd7b46886cc31
ElizavetaShavlova <eliza260926@gmail.com>
2022-05-17 00:12:04 +0300

    add tests: 1) get specific user 2)  filter user list on page 5  by gender & status

--- test/users.js
+++ test/users.js
@@ -6,7 +6,7 @@ import { expect } from 'chai';
 const TOKEN = 
     'a45e357c080a761f2194bc699f5ef27f1311fdaa50ce8af5658e92377bb42e54';
 
-describe('Users', () => {
+describe('List of user accounts', () => {
     it('Get /users list and verify that it is not empty (using "done" callback)', (done) => {
         request.get(`users?access-token=${TOKEN}`).end((err, res) => {
             console.log("error: ", err);
@@ -17,6 +17,20 @@ describe('Users', () => {
     it('Get /users list and verify that it is not empty (using promise)', () => {
         return request.get(`users?access-token=${TOKEN}`).then((res) => {
             expect(res.body.data).to.not.be.empty;
+        });
+    });
+    it('Get 3141-rd user and verify that it has id=3141', () => {
+        return request.get(`users/3141?access-token=${TOKEN}`).then((res) => {
+            expect(res.body.data.id).to.be.eq(3141);
+        })        
+    });
+    it('Get users with query params: female, active, from page 5', () => {
+        const URL = `users?access-token=${TOKEN}&gender=female&status=active&page=5`
+        return request.get(URL).then((res) =>{
+            res.body.data.forEach((data) => {
+                expect(data.gender).to.eq('female');
+                expect(data.status).to.eq('active');
+            });
         });
     });
 });
